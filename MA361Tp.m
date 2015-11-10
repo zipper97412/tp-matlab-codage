c9b9c58f59dda65414c2d4556ba5986def9a5474
zipper97412 <zipper97412@gmail.com>
2015-11-10 11:57:43 +0100

    Update MA361Tp.m

--- MA361Tp.m
+++ MA361Tp.m
@@ -1,38 +1,46 @@
 G = [1 0 0 0 1 1 0 ; 0 1 0 0 1 0 1 ; 0 0 1 0 0 1 1 ; 0 0 0 1 1 1 1];
-Ht = [1 1 0 ; 1 0 1 ; 0 1 1 ; 1 1 1 ; 1 0 0 ; 0 1 0 ; 0 0 1];
-iB = 0;
-ib = 0;
+Ht = [1 1 0 ; 1 0 1 ; 0 1 1 ; 1 1 1 ; 1 0 0 ; 0 1 0 ; 0 0 1]; %transposé de H
+iB = 0; %compteur d'erreur bloc
+ib = 0; %compteur d'erreur bit
 pB = [];
 pb = [];
-n = 5000;
+n = 50000;
 pas = 0.01;
-%s = [0 0 1 1];
-%c = mod(s*G,2);
-%s = round(rand(1,4))
+f = [0:pas:1];
 
-for f=0:pas:1
+for i=[1:1/pas+1] %on fait une boucle sur f
     iB = 0;
     ib = 0;
-    for p=1:n
+    for p=1:n %on fait l'experience sur 4bits a chaque fois
+        %création de mot de 4 bits
         s = round(rand(1,4));
+        %encodage du mots par hamming
         c = mod(s*G,2);
 
-        r = canal(c, f);
+        %passage par le canal
+        r = canal(c, f(i));
+        
+        %correction de la sortie canal
         S = mod(r*Ht,2);
         rc = correc(r, S, Ht);
         m = rc(1:4);
 
+        %comptage des bits/blocs faux
         x = mean(xor(s,m));
         if x ~= 0
             iB = iB + 1;
             ib = ib + round(x*4);
         end
     end
+    %ajout des pb/pB dans un tableau pour l'affichage de la courbe
     pB = [pB (iB/n)];
     pb = [pb (ib/(4*n))];
-    %disp(['pB : ' num2str(iB/n)]);
-    %disp(['pb : ' num2str(ib/(4*n))]);
 end
 
+%affichage pour les differents f
+figure
 plot([0:pas:1],pB,[0:pas:1],pb)
-%plot([0:pas:1],pb)
+title('pB(f) et pb(f)');
+xlabel('f');
+ylabel('pB, pb');
+legend('pB', 'pb');
